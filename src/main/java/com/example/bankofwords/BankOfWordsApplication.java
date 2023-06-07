2db5b821cd706637e0967bd898b45f58a2244b52
nikasagina <nsagi21@freeuni.edu.ge>
2023-06-07 22:03:31 +0400

    Initial commit

---
+++ src/main/java/com/example/bankofwords/BankOfWordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankofwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfWordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOfWordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankofwords/BankOfWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankofwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOfWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
