7aa33f659b888554421a1c1053054cd6711a1e54
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-16 23:33:56 +0400

    Refactor StatisticsService tests

--- src/test/java/com/example/bankofwords/service/StatisticsServiceTest.java
+++ src/test/java/com/example/bankofwords/service/StatisticsServiceTest.java
@@ -4,16 +4,16 @@ import com.example.bankofwords.dao.StatisticsDAO;
 import com.example.bankofwords.dao.UserDAO;
 import com.example.bankofwords.dao.WordDAO;
 import com.example.bankofwords.dao.WordHistoryDAO;
+import com.example.bankofwords.objects.DailyUserReport;
 import com.example.bankofwords.objects.Word;
-import com.example.bankofwords.utils.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -34,9 +34,6 @@ public class StatisticsServiceTest {
     private UserDAO userDAO;
 
     @Mock
-    private JwtUtil jwtUtil;
-
-    @Mock
     private StatisticsDAO statisticsDAO;
 
     @Mock
@@ -44,140 +41,74 @@ public class StatisticsServiceTest {
 
 
     @Test
-    void whenStartRequestWithValidToken_returnsOkResponse() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-
-        // Act
-        ResponseEntity<?> response1 = statisticsService.getUserStatistics(authHeader);
-        ResponseEntity<?> response2 = statisticsService.getGlobalStatistics(authHeader);
-        ResponseEntity<?> response3 = statisticsService.getUserActivity(authHeader);
-
-        // Assert
-        assertEquals(HttpStatus.OK, response1.getStatusCode());
-        assertEquals(HttpStatus.OK, response2.getStatusCode());
-        assertEquals(HttpStatus.OK, response3.getStatusCode());
-    }
-
-    @Test
-    void whenStartRequestWithInvalidToken_returnsUnauthorizedResponse() {
-        // Arrange
-        String authHeader = "Bearer invalidToken";
-        String username = "testUser";
-
-        when(jwtUtil.getUsernameFromToken("invalidToken")).thenReturn(username);
-        when(jwtUtil.validateToken("invalidToken", username)).thenReturn(false);
-
-        // Act
-        ResponseEntity<?> response1 = statisticsService.getUserStatistics(authHeader);
-        ResponseEntity<?> response2 = statisticsService.getGlobalStatistics(authHeader);
-        ResponseEntity<?> response3 = statisticsService.getUserActivity(authHeader);
-
-        // Assert
-        assertEquals(HttpStatus.UNAUTHORIZED, response1.getStatusCode());
-        assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
-        assertEquals(HttpStatus.UNAUTHORIZED, response3.getStatusCode());
-    }
-
-    @Test
-    void whenUserStatisticsRequestAndUserHasNotGuessed_returnsOkResponseWithEmptyValues() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
+    public void testGetUserStatistics() {
+        // Setup mock data
         long userId = 1L;
+        when(statisticsDAO.getUserTotalGuessesCount(userId)).thenReturn(10);
+        when(statisticsDAO.getUserSuccessfulGuessesCount(userId)).thenReturn(5);
+        when(statisticsDAO.getUserSuccessRateTotal(userId)).thenReturn(0.5);
+        when(statisticsDAO.getMostGuessedWordByUser(userId)).thenReturn(1L);
+        when(statisticsDAO.getLeastGuessedWordByUser(userId)).thenReturn(2L);
+        Word word1 = new Word(1L, "word1", "", 1L);
+        Word word2 = new Word(1L, "word2", "", 1L);
+        when(wordDAO.getWordWithId(1L)).thenReturn(word1);
+        when(wordDAO.getWordWithId(2L)).thenReturn(word2);
 
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(userDAO.getUserID(username)).thenReturn(userId);
+        // Execute the service call
+        Map<String, Object> userStatistics = statisticsService.getUserStatistics(userId);
+
+        // Assertions
+        assertEquals(10L, userStatistics.get("total_guesses_count"));
+        assertEquals(5L, userStatistics.get("success_guesses_count"));
+        assertEquals(0.5, userStatistics.get("success_rate"));
+        assertEquals("word1", userStatistics.get("most_guessed_word"));
+        assertEquals("word2", userStatistics.get("least_guessed_word"));
+    }
+
+    @Test
+    public void testGetUserStatisticsWithHasNotGuessedUser() {
+        // Setup mock data
+        long userId = 1L;
         when(statisticsDAO.getUserTotalGuessesCount(userId)).thenReturn(0);
         when(statisticsDAO.getUserSuccessfulGuessesCount(userId)).thenReturn(0);
         when(statisticsDAO.getUserSuccessRateTotal(userId)).thenReturn(NaN);
-        when(statisticsDAO.getMostGuessedWordByUser(userId)).thenReturn(0L);
-        when(statisticsDAO.getLeastGuessedWordByUser(userId)).thenReturn(0L);
-        when(wordDAO.getWordWithId(0)).thenReturn(null);
+        when(statisticsDAO.getMostGuessedWordByUser(userId)).thenReturn(1L);
+        when(statisticsDAO.getLeastGuessedWordByUser(userId)).thenReturn(1L);
 
-        // Act
-        ResponseEntity<?> response = statisticsService.getUserStatistics(authHeader);
+        when(wordDAO.getWordWithId(1L)).thenReturn(null);
 
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertEquals(0L, responseBody.get("total_guesses_count"));
-        assertEquals(0L, responseBody.get("success_guesses_count"));
-        assertNull(responseBody.get("success_rate"));
-        assertNull(responseBody.get("most_guessed_word"));
-        assertNull(responseBody.get("least_guessed_word"));
-    }
+        // Execute the service call
+        Map<String, Object> userStatistics = statisticsService.getUserStatistics(userId);
 
-    @Test
-    void whenUserStatisticsRequestAndOneUsersGuess_returnsOkResponseWithEmptyValues() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-        long userId = 1L;
-        Word word = new Word(1L, "example", "definition", 1L);
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(userDAO.getUserID(username)).thenReturn(userId);
-        when(statisticsDAO.getUserTotalGuessesCount(userId)).thenReturn(1);
-        when(statisticsDAO.getUserSuccessfulGuessesCount(userId)).thenReturn(1);
-        when(statisticsDAO.getUserSuccessRateTotal(userId)).thenReturn(1.0);
-        when(statisticsDAO.getMostGuessedWordByUser(userId)).thenReturn(word.getId());
-        when(statisticsDAO.getLeastGuessedWordByUser(userId)).thenReturn(word.getId());
-        when(wordDAO.getWordWithId(word.getId())).thenReturn(word);
-
-        // Act
-        ResponseEntity<?> response = statisticsService.getUserStatistics(authHeader);
-
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertEquals(1L, responseBody.get("total_guesses_count"));
-        assertEquals(1L, responseBody.get("success_guesses_count"));
-        assertEquals(1.0, responseBody.get("success_rate"));
-        assertEquals(word.getWord(), responseBody.get("most_guessed_word"));
-        assertEquals(word.getWord(), responseBody.get("least_guessed_word"));
+        // Assertions
+        assertEquals(0L, userStatistics.get("total_guesses_count"));
+        assertEquals(0L, userStatistics.get("success_guesses_count"));
+        assertNull(userStatistics.get("success_rate"));
+        assertNull(userStatistics.get("most_guessed_word"));
+        assertNull(userStatistics.get("least_guessed_word"));
     }
 
 
     @Test
     void whenUserActivityRequestAndUserHasNotGuessed_returnsOkResponseWithEmptyValues() {
         // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
         long userId = 1L;
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(userDAO.getUserID(username)).thenReturn(userId);
-        when(wordHistoryDAO.getFullDailyActivity(userId)).thenReturn(null);
+        Map<Date, DailyUserReport> activity = Map.of(Date.valueOf(LocalDate.now()), new DailyUserReport(userId));
+        when(wordHistoryDAO.getFullDailyActivity(userId)).thenReturn(activity);
 
         // Act
-        ResponseEntity<?> response = statisticsService.getUserActivity(authHeader);
+        Map<String, Object> response = statisticsService.getUserActivity(userId);
 
         // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertNull(responseBody.get("activity"));
+        assertNotNull(response);
+        assertEquals(activity, response.get("activity"));
     }
 
     @Test
     void whenGlobalStatisticsRequestAndNoOneHasGuessed_returnsOkResponseWithEmptyValues() {
         // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
         long defaultWordId = 0L;
 
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
         when(statisticsDAO.getMostGuessedWordOverall()).thenReturn(defaultWordId);
         when(statisticsDAO.getLeastGuessedWordOverall()).thenReturn(defaultWordId);
         when(wordDAO.getWordWithId(defaultWordId)).thenReturn(null);
@@ -185,29 +116,24 @@ public class StatisticsServiceTest {
         when(statisticsDAO.getTopUserIdsWithBestSuccessRate(5)).thenReturn(List.of());
 
         // Act
-        ResponseEntity<?> response = statisticsService.getGlobalStatistics(authHeader);
+        Map<String, Object> response = statisticsService.getGlobalStatistics();
 
         // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertNull(responseBody.get("most_guessed_word"));
-        assertNull(responseBody.get("least_guessed_word"));
-        assertNull(responseBody.get("top_user"));
-        assertEquals(List.of(), responseBody.get("top_5"));
+        assertNotNull(response);
+        assertNull(response.get("most_guessed_word"));
+        assertNull(response.get("least_guessed_word"));
+        assertNull(response.get("top_user"));
+        assertEquals(List.of(), response.get("top_5"));
     }
 
 
     @Test
     void whenGlobalStatisticsRequestWithOneUsersGuess_returnsOkResponseWithEmptyValues() {
         // Arrange
-        String authHeader = "Bearer someToken";
         String username = "testUser";
         long userId = 1L;
         Word word = new Word(1L, "example", "definition", 1L);
 
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
         when(statisticsDAO.getMostGuessedWordOverall()).thenReturn(word.getId());
         when(statisticsDAO.getLeastGuessedWordOverall()).thenReturn(word.getId());
         when(wordDAO.getWordWithId(word.getId())).thenReturn(word);
@@ -215,15 +141,13 @@ public class StatisticsServiceTest {
         when(statisticsDAO.getTopUserIdsWithBestSuccessRate(5)).thenReturn(List.of(userId));
         when(userDAO.getUsername(userId)).thenReturn(username);
         // Act
-        ResponseEntity<?> response = statisticsService.getGlobalStatistics(authHeader);
+        Map<String, Object> response = statisticsService.getGlobalStatistics();
 
         // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertEquals(word.getWord(), responseBody.get("most_guessed_word"));
-        assertEquals(word.getWord(), responseBody.get("least_guessed_word"));
-        assertEquals(username, responseBody.get("top_user"));
-        assertEquals(List.of(username), responseBody.get("top_5"));
+        assertNotNull(response);
+        assertEquals(word.getWord(), response.get("most_guessed_word"));
+        assertEquals(word.getWord(), response.get("least_guessed_word"));
+        assertEquals(username, response.get("top_user"));
+        assertEquals(List.of(username), response.get("top_5"));
     }
 }
