121ebeb305987f510c47ab67df3cbf122d503dbf
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-17 22:35:17 +0400

    Refactor WordService tests

--- src/test/java/com/example/bankofwords/service/WordServiceTest.java
+++ src/test/java/com/example/bankofwords/service/WordServiceTest.java
@@ -1,25 +1,18 @@
 package com.example.bankofwords.service;
 
 import com.example.bankofwords.dao.LexiconDAO;
-import com.example.bankofwords.dao.UserDAO;
 import com.example.bankofwords.dao.WordDAO;
-import com.example.bankofwords.utils.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 public class WordServiceTest {
@@ -31,173 +24,108 @@ public class WordServiceTest {
     private WordDAO wordDAO;
 
     @Mock
-    private UserDAO userDAO;
-
-    @Mock
-    private JwtUtil jwtUtil;
-
-    @Mock
     private LexiconDAO lexiconDAO;
 
 
     @Test
-    void whenRequestsWithInvalidToken_returnsUnauthorizedResponse() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(false);
-
-        // Act
-        ResponseEntity<?> response1 = wordService.learn(authHeader, 0L);
-        ResponseEntity<?> response2 = wordService.delete(authHeader, 0L);
-        ResponseEntity<?> response3 = wordService.getDefinitions(authHeader, "");
-        ResponseEntity<?> response4 = wordService.getWordInfo(authHeader, "");
-
-        // Assert
-        assertEquals(HttpStatus.UNAUTHORIZED, response1.getStatusCode());
-        assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
-        assertEquals(HttpStatus.UNAUTHORIZED, response3.getStatusCode());
-        assertEquals(HttpStatus.UNAUTHORIZED, response4.getStatusCode());
-    }
-
-    @Test
-    void whenLearnRequest_returnsOkResponseWithSuccess() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-        long userId = 1L;
+    public void learn_shouldReturnFalseIfAlreadyKnows() {
+        // given
         long wordId = 1L;
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(userDAO.getUserID(username)).thenReturn(userId);
-        when(wordDAO.alreadyKnows(userId, wordId)).thenReturn(false);
-
-        // Act
-        ResponseEntity<?> response = wordService.learn(authHeader, wordId);
-
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assert responseBody != null;
-        assertEquals(true, responseBody.get("success"));
-    }
-
-    @Test
-    void whenLearnRequest_returnsOkResponseWithSuccessFalse() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
         long userId = 1L;
-        long wordId = 1L;
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(userDAO.getUserID(username)).thenReturn(userId);
         when(wordDAO.alreadyKnows(userId, wordId)).thenReturn(true);
 
-        // Act
-        ResponseEntity<?> response = wordService.learn(authHeader, wordId);
+        // when
+        boolean result = wordService.learn(wordId, userId);
 
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assert responseBody != null;
-        assertEquals(false, responseBody.get("success"));
+        // then
+        assertFalse(result);
+        verify(wordDAO, never()).learnWord(userId, wordId);
     }
 
     @Test
-    void whenDeleteRequest_returnsOkResponse() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
+    public void learn_shouldReturnTrueAndLearnWord() {
+        // given
+        long wordId = 1L;
+        long userId = 1L;
+        when(wordDAO.alreadyKnows(userId, wordId)).thenReturn(false);
+
+        // when
+        boolean result = wordService.learn(wordId, userId);
+
+        // then
+        assertTrue(result);
+        verify(wordDAO).learnWord(userId, wordId);
+    }
+
+    @Test
+    public void deleteWord_shouldCallDeleteWordMethod() {
+        // given
         long wordId = 1L;
 
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
+        // when
+        wordService.delete(wordId);
 
-        // Act
-        ResponseEntity<?> response = wordService.delete(authHeader, wordId);
-
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        // then
+        verify(wordDAO).deleteWord(wordId);
     }
 
     @Test
-    void whenGetDefinitionsRequest_returnsOkResponseWithDefinitions() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-        String word = "testWord";
-        List<String> definitions = List.of("definition1", "definition2");
+    public void getDefinitions_shouldReturnDefinitions() {
+        // given
+        String word = "example";
+        List<String> expected = List.of("a thing characteristic of its kind or illustrating a general rule", "a printed or written problem or exercise designed to illustrate a rule");
 
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(lexiconDAO.getDefinitions(word)).thenReturn(definitions);
+        when(lexiconDAO.getDefinitions(word)).thenReturn(expected);
 
-        // Act
-        ResponseEntity<?> response = wordService.getDefinitions(authHeader, word);
+        // when
+        List<String> result = wordService.getDefinitions(word);
 
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertEquals(definitions, responseBody.get("available_definitions"));
+        // then
+        assertEquals(expected, result);
     }
 
     @Test
-    void whenGetWordInfoRequestWithNoDefinitionsAvailable_returnsNoContent() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-        String word = "testWord";
-        String definition = "";
+    public void getWordInfo_shouldReturnWordInfo() {
+        // given
+        String word = "example";
+        String definition = "a thing characteristic of its kind or illustrating a general rule";
+        List<String> otherDefinitions = new ArrayList<>(List.of("a printed or written problem or exercise designed to illustrate a rule"));
+        List<String> examples = new ArrayList<>(Arrays.asList("for example", "give an example", "set an example"));
+        List<String> synonyms = new ArrayList<>(Arrays.asList("model", "exemplar", "typical case"));
+        List<String> antonyms = new ArrayList<>(List.of("counterexample"));
 
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
         when(lexiconDAO.getWordDefinition(word)).thenReturn(definition);
-
-        // Act
-        ResponseEntity<?> response = wordService.getWordInfo(authHeader, word);
-
-        // Assert
-        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
-    }
-
-    @Test
-    void whenGetWordInfoRequest_returnsOkResponse() {
-        // Arrange
-        String authHeader = "Bearer someToken";
-        String username = "testUser";
-        String word = "testWord";
-        String definition = "definition1";
-        List<String> definitions = new ArrayList<>(Arrays.asList("definition1", "definition2"));
-        List<String> examples = List.of("example1", "example2");
-        List<String> synonyms = List.of("synonym1", "synonym2");
-        List<String> antonyms = List.of("antonym1", "antonym2");
-
-        when(jwtUtil.getUsernameFromToken("someToken")).thenReturn(username);
-        when(jwtUtil.validateToken("someToken", username)).thenReturn(true);
-        when(lexiconDAO.getWordDefinition(word)).thenReturn(definition);
-        when(lexiconDAO.getDefinitions(word)).thenReturn(definitions);
+        when(lexiconDAO.getDefinitions(word)).thenReturn(otherDefinitions);
         when(lexiconDAO.getDefinitionExamples(word, definition)).thenReturn(examples);
         when(lexiconDAO.getWordSynonyms(word)).thenReturn(synonyms);
         when(lexiconDAO.getWordAntonyms(word)).thenReturn(antonyms);
 
-        // Act
-        ResponseEntity<?> response = wordService.getWordInfo(authHeader, word);
+        Map<String, Object> expected = new HashMap<>();
+        expected.put("word", word);
+        expected.put("definition", definition);
+        expected.put("other_definitions", otherDefinitions);
+        expected.put("examples", examples);
+        expected.put("synonyms", synonyms);
+        expected.put("antonyms", antonyms);
 
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        assertNotNull(responseBody);
-        assertEquals(word, responseBody.get("word"));
-        assertEquals(definition, responseBody.get("definition"));
-        assertEquals(List.of("definition2"), responseBody.get("other_definitions"));
-        assertEquals(examples, responseBody.get("examples"));
-        assertEquals(synonyms, responseBody.get("synonyms"));
-        assertEquals(antonyms, responseBody.get("antonyms"));
+        // when
+        Map<String, Object> result = wordService.getWordInfo(word);
+
+        // then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void getWordInfo_shouldReturnNullIfNoDefinition() {
+        // given
+        String word = "example";
+
+        when(lexiconDAO.getWordDefinition(word)).thenReturn("");
+
+        // when
+        Map<String, Object> result = wordService.getWordInfo(word);
+
+        // then
+        assertNull(result);
     }
 }
